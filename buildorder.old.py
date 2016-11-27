6fd24dc2d600d2b81ad26e50ff88fd6a8839a86c
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-11-27 15:53:22 -0700

    fixed orders printing wrong items

--- buildorder.old.py
+++ buildorder.old.py
@@ -41,7 +41,6 @@ def main():
 
     orderJSON, arabicImageLinks = readJSON(argIn)
     writeHTML(orderJSON)
-    #  makeHTML(orderJSON)
 
 def getOrderNums(json):
     """
@@ -88,7 +87,7 @@ def makeOneSided(json):
     # create the keys, from unique identifier.
     for item in json:
         if not any(item['type'].lower() in s for s in bandTypes) and item['attr1'].lower().replace(' ','') != '2sided':
-            identifier = item['itemSort']
+            identifier = item['itemSort'] + item['type'].lower()
             itemOneSided[unidecode(identifier)] = []
 
     for item in json:
@@ -98,9 +97,9 @@ def makeOneSided(json):
                 buildItem += "<tr><td class='symbol " + item['side1lang'].lower() + "'>" + letter + "</td></tr>"
             buildItem += "</tbody></table><div class='itemDescription'>"
             buildItem += "<div class='size arabic'><img src='.img/" + str(item['size']) + ".jpg'></div>"
-            buildItem += "<div class='description'> " + item['itemNum'] + " " + str(item['size']) + "<br>" + item['label'] + "<br>"
+            buildItem += "<div class='description'> " + item['itemNum'] + "<br>" + "SIG -" + item['label'] + "<br>"
             buildItem += str(item['orderNum']) + "<br></div></div></div>"
-            identifier = item['itemSort']
+            identifier = item['itemSort'] + item['type'].lower()
             itemOneSided[unidecode(identifier)].append(buildItem)
 
     return itemOneSided
@@ -129,7 +128,7 @@ def makeTwoSided(json):
                 buildItem += "<td class='side2 symbol " + item['side2lang'].lower() + "'>" + side2.upper() + "</td></tr>"
             buildItem += "</tbody></table><div class='itemDescription'>"
             buildItem += "<div class='size arabic'><img src='.img/" + str(item['size']) + ".jpg'></div>"
-            buildItem += "<div class='description'>" + item['itemNum'] + " " + item['size'] + "<br>" + item['label'] + "<br>"
+            buildItem += "<div class='description'>" + item['itemNum'] + "<br>" + "SIG -" + item['label'] + "<br>"
             buildItem += str(item['orderNum']) + "<br></div></div></div>"
             itemTwoSided[item['itemSort']].append(buildItem)
 
@@ -153,33 +152,26 @@ def makeBand(json):
     # create values
     for item in json:
         if any(item['type'].lower() in s for s in bandTypes):
+            itemBand[item['itemSort']] = []
             buildItem = "<div class='item band'><div><table class='itemSymbols'><tbody><tr>"
             for letter in item['side1symb']:
                 buildItem += "<td class='symbol " + item['side1lang'].lower() + "'>" + letter + "</td>"
             buildItem += "<td class='blank'></td>"
             buildItem += "<td class='size arabic'><img src='.img/" + str(item['size']).lower() + ".jpg'></td>"
             buildItem += "</tbody></table></div><div class='itemDescription'>"
-            buildItem += "<div class='description'>" + item['itemNum'] + str(item['size']) + " " + item['label'] + " "
+            buildItem += "<div class='description'>" + item['itemNum'] + " SIG -" + item['label'] + " "
             buildItem += str(item['orderNum']) + "</div></div></div>"
             itemBand[item['itemSort']].append(buildItem)
 
     return itemBand
 
 def makeHTML(json):
-    """
-    build out the whole html page
-    loop through all of the previously created html information and create a full html page based on that information
-
-    oneSideds, twoSideds, bands, orders = {
-        "identifier" : "html string"
-    }
-    """
+    """build out the whole html page"""
 
     orders = getOrderNums(json) 
     oneSideds = makeOneSided(json)
     twoSideds = makeTwoSided(json)
     bands = makeBand(json)
-    print bands
     sortOrder = sorted(orders.items())
 
     boilerplate = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title></title><style> @font-face {  font-family: 'heiro';src: url('.fonts/Hiero.ttf');}@font-face {font-family: 'astro';src: url('.fonts/Astro.ttf');}@font-face {font-family: 'greek';src: url('.fonts/greek.ttf');}.hiero {font-family: 'heiro';font-size: 30px;}.astro {font-family: 'astro';font-size: 30px;}.greek {font-family: 'greek';font-size: 30px;}div.orderDescData {max-height: 50px;margin-bottom: 10px;}span.arabic > img {max-height: 50px;max-width: 150px;}div.item {float: left;display: flex-inline;height: 445px;border-right: 1px solid black;margin-bottom: 20px;}div.item.oneSided {width: 85px;}div.item.twoSided {width: 115px;}div.size > img {display: block;margin: 0 auto;}div.orderDescData:last-child {clear: right;}table.itemSymbols {width: 100%;}td.symbol {width: 30px;height: 30px;text-align: center;}div.item {position: relative;}div.itemDescription {position: absolute;bottom: 0;max-width: 100%;overflow: hidden;white-space: nowrap;}div.band {height: 90px;width: auto;}div.description {margin: 10px;}div.break {clear: both;}</style></head><body>"
@@ -190,39 +182,44 @@ def makeHTML(json):
     x1, x2, x3 = 0, 0, 0 # for counting orders to insert line breaks
     # create OneSided
     for key, order in sortOrder: 
-        html += "<div class='break'></div>"
-        html += "<div class='order'>"
-        o = 0 
-        for oneSided in oneSideds:
+        if 'ring' not in key and 'bracelet' not in key and '2sided' not in key and x1 == 0:
+            html += "<div class='break'></div>"
+            html += "<div class='order'>"
+            html += order
             o = 0 
-            if oneSided.lower() in key.lower():
-                html += "<div class='break'></div>"
-                html += order
+            for oneSided in oneSideds:
                 for item in oneSideds[oneSided]:
                     if o % 8 == 0 and o != 0:
                         html += "<div class='break'></div>" + order
                     html += item 
                     o += 1
-        t = 0
-        for twoSided in twoSideds:
+            html += "</div>"
+            x1 += 1
+        # create TwoSided
+        if 'ring' not in key and 'bracelet' not in key and '2sided' in key and x2 == 0:
+            html += "<div class='break'></div>"
+            html += "<div class='order'>"
+            html += order
             t = 0
-            if twoSided.lower() in key.lower():
-                html += "<div class='break'></div>"
-                html += order
+            for twoSided in twoSideds:
                 for item in twoSideds[twoSided]:
-                    if t % 6 == 0 and t != 0 :
+                    if t % 6 == 0 and t != 0:
                         html += "<div class='break'></div>" + order
-                    html += item 
+                    html += item
                     t += 1
-        for band in bands:
-            if band.lower() in key.lower():
+            html += "</div>"
+            x2 += 1
+        # create Band
+        if 'ring' in key or 'bracelet' in key and x3 == 0:
+            html += "<div class='break'></div>"
+            for band in bands:
                 for item in bands[band]:
-                    html += "<div class='break'></div>" + order
-                    html += item 
-
+                    html += "<div class='break'<div class='order'>"
+                    html += order
+                    html += item
+            x3 += 1
     html += endplate
 
-    #  print html
     return html
 
 def writeHTML(json):
